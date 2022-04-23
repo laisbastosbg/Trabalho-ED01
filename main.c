b794e792276d792701637413b55e1fac571c5325
Lais Godinho <47223072+laisbastosbg@users.noreply.github.com>
2022-04-23 01:05:05 +0000

    infix to postfix working

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ typedef struct Node {
   struct Node *tail;
 } Node;
 
-void addToEnd(Node **head, char value) {
+void add(Node **head, char value) {
   Node *tmp = *head;
   Node *node = (Node *)malloc(sizeof(Node));
 
@@ -30,34 +30,6 @@ void addToEnd(Node **head, char value) {
   node->previous = tmp;
 }
 
-void add(Node **head, char value, Node *lastNode) {
-  Node *tmp = *head;
-  Node *node = (Node *)malloc(sizeof(Node));
-
-  node->value = value;
-  node->next = NULL;
-  node->head = *head;
-  node->tail = node;
-
-  while (tmp != lastNode) {
-    if(lastNode->next == NULL) {
-      tmp->tail = node;
-    }
-    
-    tmp = tmp->next;
-  }
-
-  if(tmp->next) {
-    node->next = tmp->next;
-  }
-  
-  tmp->next = node;
-  // if(lastNode->next == NULL) {
-    tmp->tail = node;
-  // }
-  node->previous = tmp;
-}
-
 void pop(Node **head, Node *node) {
 
   if (node->previous) {
@@ -70,9 +42,7 @@ void pop(Node **head, Node *node) {
 
   Node *tmp = *head;
   while (tmp->next) {
-    if(node->next == NULL) {
-      tmp->tail = node->previous;
-    }
+    tmp->tail = node->previous;
     tmp = tmp->next;
   }
 
@@ -89,7 +59,7 @@ void list(Node *head) {
   printf("\n");
 }
 
-char *validateBrackets(Node *head) {
+void validateBrackets(Node *head) {
   int openingBracketCounter = 0;
   int closingBracketCounter = 0;
   Node *tmp = head;
@@ -101,17 +71,20 @@ char *validateBrackets(Node *head) {
     if (tmp->value == ')')
       closingBracketCounter++;
 
-    if (closingBracketCounter > openingBracketCounter)
-      return "Error: closing brackets must come after opening brackets\n";
+    if (closingBracketCounter > openingBracketCounter) {
+      printf("Error: closing brackets must come after opening brackets\n");
+      exit(0);
+    }
   }
 
-  if (closingBracketCounter != openingBracketCounter)
-    return "Error: brackets count not matched\n";
-
-  return "ok\n";
+  if (closingBracketCounter != openingBracketCounter){
+    
+    printf("Error: brackets count not matched\n");
+    exit(0);
+  }
 }
 
-char *validateOperators(Node *head) {
+void validateOperators(Node *head) {
   Node *tmp = head->next;
 
   char currentElement = tmp->value;
@@ -119,7 +92,8 @@ char *validateOperators(Node *head) {
 
   if (currentElement == '+' || currentElement == '-' || currentElement == '*' ||
       currentElement == '/') {
-    return "Error: Invalid operation.\n";
+    printf("Error: Invalid operation.\n");
+    exit(0);
   }
 
   while (tmp->next) {
@@ -128,7 +102,8 @@ char *validateOperators(Node *head) {
 
     if (tmp->next == NULL && (tmp->value == '+' || tmp->value == '-' ||
                               tmp->value == '*' || tmp->value == '/')) {
-      return "Error: Invalid operation.\n";
+      printf("Error: Invalid operation.\n");
+      exit(0);
     }
 
     if ((currentElement == '+' || currentElement == '-' ||
@@ -137,49 +112,17 @@ char *validateOperators(Node *head) {
 
         (nextElement == '+' || nextElement == '-' || nextElement == '*' ||
          nextElement == '/' || nextElement == ')')) {
-      return "Error: Invalid operation\n";
+      printf("Error: Invalid operation\n");
+      exit(0);
     }
     tmp = tmp->next;
   }
 
   if (tmp->value == '+' || tmp->value == '-' || tmp->value == '*' ||
       tmp->value == '/') {
-    return "Error: Invalid operation.\n";
-  }
-
-  return "ok\n";
-}
-
-char *validateCharacters(Node *head) {
-  Node *tmp = head;
-
-  while (tmp->next) {
-    tmp = tmp->next;
-
-    switch (tmp->value) {
-    case '0':
-    case '1':
-    case '2':
-    case '3':
-    case '4':
-    case '5':
-    case '6':
-    case '7':
-    case '8':
-    case '9':
-    case '+':
-    case '-':
-    case '*':
-    case '/':
-    case '(':
-    case ')':
-      break;
-    default:
-      return "Error: Invalid input\n";
+    printf("Error: Invalid operation.\n");
+    exit(0);
     }
-  }
-
-  return "ok\n";
 }
 
 void stripSpaces(Node *head) {
@@ -217,74 +160,26 @@ Node *parseToPostFix(Node *head) {
   while (tmp->next) {
     tmp = tmp->next;
     if (tmp->value == '(') {
-      add(&bracketStackHead, tmp->value, bracketStackHead->tail);
-    } 
-    else if (tmp->value == ')') {
+      add(&bracketStackHead, tmp->value);
+    } else if (tmp->value == ')') {
       while (operatorStackHead->next) {
-        add(&newStackHead, operatorStackHead->tail->value, newStackHead->tail);
+        add(&newStackHead, operatorStackHead->tail->value);
         pop(&operatorStackHead, operatorStackHead->tail);
         pop(&bracketStackHead, bracketStackHead->tail);
       }
-    } 
-    else if (isdigit(tmp->value)) {
-      add(&newStackHead, tmp->value, newStackHead->tail);
-    } 
-    else if (tmp->value == '+' || tmp->value == '-' || tmp->value == '*' ||
+    } else if (isdigit(tmp->value)) {
+      add(&newStackHead, tmp->value);
+    } else if (tmp->value == '+' || tmp->value == '-' || tmp->value == '*' ||
                tmp->value == '/') {
-      // printf("%c\n", operatorStackHead->tail->value);
-      // if(operatorStackHead->tail) {
-      // addToEnd(&operatorStackHead, tmp->value);
-      // }
-      
+      add(&operatorStackHead, tmp->value);
     }
   }
-  // while (operatorStackHead->next) {
-  //   add(&newStackHead, operatorStackHead->tail->value, newStackHead->tail);
-  //   pop(&operatorStackHead, operatorStackHead->tail);
-  // }
-  // printf()
+  while (operatorStackHead->next) {
+    add(&newStackHead, operatorStackHead->tail->value);
+    pop(&operatorStackHead, operatorStackHead->tail);
+  }
+  
   return newStackHead;
-}
-
-int isOperator(char character) {
-  if (
-    character == '+' ||
-    character == '-' ||
-    character == '*' ||
-    character == '/'
-  ) return 1;
-
-  return 0;
-}
-
-int calculate(Node *head) {
-  Node *tmp = head;
-  int result = 0;
-  while(tmp->next) {
-    if(isOperator(tmp->value) == 1) {
-      int a = tmp->previous->previous->value;
-      int b = tmp->previous->value;
-
-      printf("\n%c %c %c\n", a, tmp->value, b);
-      if(tmp->value == '+') {
-        result += (a + b);
-      } else if(tmp->value == '-') {
-        result += (a - b);
-      } else if(tmp->value == '/') {
-        result += (a / b);
-      } else if(tmp->value == '*') {
-        result += (a * b);
-      }
-
-      pop(&tmp, tmp);
-      pop(&tmp, tmp->previous);
-      pop(&tmp, tmp->previous->previous);
-    }
-
-    tmp = tmp->next;
-  }
-
-  return result;
 }
 
 int main() {
@@ -299,36 +194,24 @@ int main() {
   size_t length = 0;
   ssize_t read;
 
-  // read = getline(&entrada, &length, stdin);
-  // printf("-> %s", entrada);
+  printf("enter infix expression: ");
+  read = getline(&entrada, &length, stdin);
 
   // coloca a string de entrada em uma lista encadeada
   int i;
-  entrada = "((2 + 3) * (5 + 2) ) * ((3 + 8 - 2 )  * (2 + 3)  * (3 + 4) )";
-  // entrada = "2+1-3*4";
   for (i = 0; i < strlen(entrada); i++) {
-    // printf("%d: %c\n", i, entrada[i]);
-    add(&head, entrada[i], head->tail);
+    add(&head, entrada[i]);
   }
-  char *validateBracketsResponse = validateBrackets(head);
 
-  // printf("%s\n", validateBracketsResponse);
+  validateBrackets(head);
 
-  char *validateOpeatorsResponse = validateOperators(head);
+  validateOperators(head);
 
-  // printf("%s\n", validateOpeatorsResponse);
-
-  char *validateCharactersResponse = validateCharacters(head);
-
-  // printf("%s\n\n\n", validateCharactersResponse);
-  // list(head);
   stripSpaces(head);
-// printf("tail: %c\n", head->tail->value);
-  list(head);
 
   Node *postfix = parseToPostFix(head);
-list(postfix);
-//   printf("\nresult: %d\n", calculate(postfix));
+  printf("postfix: ");
+  list(postfix);
 
   return 0;
 }
